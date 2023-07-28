35eda363ed318bac7f6fcfcb556424111d8aa254
Eduardo0409 <du.caldas.alves@gmail.com>
2023-07-28 16:22:12 -0300

    Projeto finalizado

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Basket from '../views/Basket.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/basket',
+    name: 'Basket',
+    component: Basket
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
